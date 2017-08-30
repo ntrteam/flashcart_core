ea385ec89871f334a5d3bb5d03b1524cd0b19943
Matt Stevens <matt@handsomematt.co.uk>
2017-08-30 14:20:35 +0100

    add example device

---
+++ devices/example.cpp
@@ -0,0 +1,29 @@
+#ifdef DONT_INCLUDE_EXAMPLE_PLEASE
+
+#include "device.h"
+
+class Example : Flashcart {
+    public:
+        // Name & Size of Flash Memory
+        Example() : Flashcart("Example Name", 0x400000) { }
+
+        /*
+        called when the user selects this flashcart
+        return false to error
+        return true otherwise
+        */
+        bool initialize() {
+            return true;
+        }
+
+        void shutdown() { }
+        
+        void readFlash(uint32_t address, uint32_t length, uint8_t *buffer) { }
+        void writeFlash(uint32_t address, uint32_t length, const uint8_t *buffer) { }
+        void injectNtrBoot(uint8_t *blowfish_key, uint8_t *firm, uint32_t firm_size) { }
+};
+
+// adds your cart to the list
+Example example;
+
+#endif
