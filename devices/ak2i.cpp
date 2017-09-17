d972106428594cbe085715b4621b657df7f5f75a
Kitlith <kitlith@kitl.pw>
2017-09-16 23:54:31 -0700

    Minor changes/fixes for various carts.

--- devices/ak2i.cpp
+++ devices/ak2i.cpp
@@ -108,7 +108,7 @@ protected:
 public:
     AK2i() : Flashcart("Acekard 2i", 0x200000) { }
 
-    const char *getAuthor() { return "kitling + normmatt"; }
+    const char *getAuthor() { return "Kitlith + Normmatt"; }
     const char *getDescription() { return "Works with the following carts:\n * Acekard 2i HW-44\n * Acekard 2i HW-81\n * R4i Ultra (r4ultra.com)"; }
 
     const size_t getMaxLength()
