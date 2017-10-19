32a3a07d521a12c89ddf8eca8ce84b0e192e7c70
angelsl <hidingfromhidden@gmail.com>
2017-10-20 01:23:21 +0800

    Update example flashcart for Flashcart class changes
    It's actually useful to test ntrboot_flasher UI

--- devices/example.cpp
+++ devices/example.cpp
@@ -14,7 +14,6 @@ class Example : Flashcart {
 
         const char* getAuthor() { return "your name"; }
         const char* getDescription() { return "something helpful\nuse\newlines"; }
-    
 
         /*
         called when the user selects this flashcart
@@ -26,10 +25,10 @@ class Example : Flashcart {
         }
 
         void shutdown() { }
-        
-        void readFlash(uint32_t address, uint32_t length, uint8_t *buffer) { }
-        void writeFlash(uint32_t address, uint32_t length, const uint8_t *buffer) { }
-        void injectNtrBoot(uint8_t *blowfish_key, uint8_t *firm, uint32_t firm_size) { }
+
+        bool readFlash(uint32_t address, uint32_t length, uint8_t *buffer) { return true; }
+        bool writeFlash(uint32_t address, uint32_t length, const uint8_t *buffer) { return true; }
+        bool injectNtrBoot(uint8_t *blowfish_key, uint8_t *firm, uint32_t firm_size) { return true; }
 };
 
 // adds your cart to the list
